214917a5c2b6c9ddd342e39f2a4cf007f320782c
Iaml1mon <75097283+Iaml1mon@users.noreply.github.com>
2025-04-24 04:26:49 +1000

    fix: Add App.css import and resolve CSS conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { LandingPageProvider } from "./LandingPage/LandingPageContext.jsx";
-import { BookingStepsProvider } from "./BookingSteps/BookingStepsContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LandingPageProvider>
-      <BookingStepsProvider>
-        <App />
-      </BookingStepsProvider>
-    </LandingPageProvider>
+    <App />
   </React.StrictMode>
 );
